cd43ffc95616e0188d41e59594881ee773169703
NadirAkram <97799221+NadirAkram@users.noreply.github.com>
2024-12-28 12:45:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,12 +14,17 @@ document.getElementById("search").onclick = function() {
 	const yourArray = tags.split(",");
 	const count = yourArray.length;
 	const list = [];
+	const list2 = [];
 	for (let i = 0; i < count; i++) {
 		const string1 = `<condition test="contains" flags="74"> <category> <name internal="LcRevitData_Element">Element</name> </category> <property> <name internal="lcldrevit_parameter_Raceway Sequence ID_PG_IDENTITYDATA">Tag Number</name> </property> <value> <data type="wstring">${yourArray[i]}</data> </value> </condition>`;
 		list.push(string1);
 	}
 	const newString = list.join("\n");
-	const fromtostring = `<condition test="contains" flags="74"> <category> <name internal="LcRevitData_Element">Element</name> </category> <property> <name internal="LcRevitPropertyElementName">Name</name> </property> <value> <data type="wstring">${fromToArray[0]}</data> </value> </condition> <condition test="contains" flags="74"> <category> <name internal="LcRevitData_Element">Element</name> </category> <property> <name internal="LcRevitPropertyElementName">Equipment Number</name> </property> <value> <data type="wstring">${fromToArray[1]}</data> </value> </condition>`
+	for (let j = 0; j < count; j++) {
+		const string2 = `<condition test="contains" flags="74"> <category> <name internal="LcRevitData_Element">Element</name> </category> <property> <name internal="LcRevitPropertyElementName">Equipment Number</name> </property> <value> <data type="wstring">${fromToArray[j]}</data> </value> </condition>`;
+		list2.push(string2);
+	}
+	const fromtostring = list2.join("\n");
 	const finalString = `<?xml version="1.0" encoding="UTF-8" ?> <exchange xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://download.autodesk.com/us/navisworks/schemas/nw-exchange-12.0.xsd" units="ft" filename="" filepath=""> <findspec mode="all" disjoint="0"> <conditions>${newString}${fromtostring} </conditions> <locator>/</locator> </findspec> </exchange>`;
 	document.getElementById("myInput").textContent = `${finalString}`
 }
