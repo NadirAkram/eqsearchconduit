7fbee1963eb022fedaef521713ff9284a8fc7a26
NadirAkram <97799221+NadirAkram@users.noreply.github.com>
2025-04-28 11:27:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,15 +7,20 @@ function myFunction() {
 }
 let tags;
 let from;
+let cableTag;
 document.getElementById("search").onclick = function() {
 	tags = document.getElementById("tag").value;
 	from = document.getElementById("from").value;
+ cableTag = document.getElementById("cableTag").value;
 	const fromToArray = from.split("+");
 	const yourArray = tags.split(",");
+ const yourArray2 = tags.split(",");
 	const count = yourArray.length;
 	const count2 = fromToArray.length;
+ const count3 = yourArray2.length;
 	const list = [];
 	const list2 = [];
+ const list3 = [];
 	for (let i = 0; i < count; i++) {
 		const string1 = `<condition test="contains" flags="74"> <category> <name internal="LcRevitData_Element">Element</name> </category> <property> <name internal="lcldrevit_parameter_Raceway Sequence ID_PG_IDENTITYDATA">Tag Number</name> </property> <value> <data type="wstring">${yourArray[i]}</data> </value> </condition>`;
 		list.push(string1);
@@ -26,6 +31,13 @@ document.getElementById("search").onclick = function() {
 		list2.push(string2);
 	}
 	const fromtostring = list2.join("\n");
-	const finalString = `<?xml version="1.0" encoding="UTF-8" ?> <exchange xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://download.autodesk.com/us/navisworks/schemas/nw-exchange-12.0.xsd" units="ft" filename="" filepath=""> <findspec mode="all" disjoint="0"> <conditions>${newString}${fromtostring} </conditions> <locator>/</locator> </findspec> </exchange>`;
+
+for (let k = 0; k < count3; k++) {
+		const string3 = `<condition test="contains" flags="74"> <category> <name internal="LcRevitData_Element">Element</name> </category> <property> <name internal="LcRevitPropertyElementName">Cable Tag</name> </property> <value> <data type="wstring">${yourArray2[k]}</data> </value> </condition>`;
+		list3.push(string3);
+	}
+	const cableTagString = list3.join("\n");
+
+	const finalString = `<?xml version="1.0" encoding="UTF-8" ?> <exchange xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://download.autodesk.com/us/navisworks/schemas/nw-exchange-12.0.xsd" units="ft" filename="" filepath=""> <findspec mode="all" disjoint="0"> <conditions>${newString}${fromtostring}${cableTagString} </conditions> <locator>/</locator> </findspec> </exchange>`;
 	document.getElementById("myInput").textContent = `${finalString}`
 }
